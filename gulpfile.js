fc20134c7ac2670439b66fe070b3f4474711a42d
Aj Castiglione <antoniocastiglione10@gmail.com>
2018-07-02 12:12:44 -0400

    moved main app scss file to root of the src folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,23 @@
-const gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    autoprefixer = require('gulp-autoprefixer');
+const gulp = require("gulp"),
+  sass = require("gulp-sass"),
+  autoprefixer = require("gulp-autoprefixer");
 
-gulp.task('default', function () {
-    gulp.watch('src/scss/*.scss', gulp.series('styles'));
+gulp.task("default", function() {
+  gulp.watch("src/scss/*.scss", gulp.series("styles"));
 });
 
-gulp.task('styles', function () {
-    return gulp.src('src/scss/*.scss')
-        .pipe(sass({
-            outputStyle: 'compressed'
-        }).on('error', sass.logError))
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions']
-        }))
-        .pipe(gulp.dest('./src'));
+gulp.task("styles", function() {
+  return gulp
+    .src("src/*.scss")
+    .pipe(
+      sass({
+        outputStyle: "compressed"
+      }).on("error", sass.logError)
+    )
+    .pipe(
+      autoprefixer({
+        browsers: ["last 2 versions"]
+      })
+    )
+    .pipe(gulp.dest("./src"));
 });
